c4847497bb0afc61c2552dcb9b00d3c0d37249f7
Kaaeed <stoobideak4@gmail.com>
2024-01-16 12:55:43 +0100

    Added headers and meta descriptions

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,20 @@ import { Fragment } from "react";
 import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 function HomePage(props) {
   const { featuredPosts } = props;
 
   return (
     <Fragment>
+      <Head>
+        <title>Welcome to my blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={featuredPosts} />
     </Fragment>
