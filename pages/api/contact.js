c413e5143760f25afcf9a0a4771a6d0c9f4a5062
Kaaeed <stoobideak4@gmail.com>
2024-01-16 12:18:28 +0100

    Added contact form features and connected it to the mongoDB

---
+++ pages/api/contact.js
@@ -0,0 +1,58 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+
+    // store it in a database
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://nextjsblog:nextjsblog@nextjs-blog.rakgjgb.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to the database." });
+      return;
+    }
+
+    const db = client.db();
+
+    let result;
+    try {
+      result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing the message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res.status(201).json({
+      message: "Successfully stored the message!",
+      message: newMessage,
+    });
+  }
+}
+
+export default handler;
